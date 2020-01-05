f47a00cabe2c4569c9dbc981ca407419d1921cac
Pathik <pathik119@gmail.com>
2020-01-05 10:30:25 +0000

    again to basic

--- vpcmodule.tf
+++ vpcmodule.tf
@@ -1,4 +1,32 @@
 ##################################################################################
+# VARIABLES
+##################################################################################
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "private_key_path" {}
+variable "key_name" {}
+variable "region" {
+  default = "us-east-2"
+}
+variable "network_address_space" {
+  default = "10.1.0.0/16"
+}
+variable "subnet1_address_space" {
+  default = "10.1.0.0/24"
+}
+variable "subnet2_address_space" {
+  default = "10.1.1.0/24"
+}
+variable "subnet3_address_space" {
+  default = "10.1.2.0/24"
+}
+variable "subnet4_address_space" {
+  default = "10.1.3.0/24"
+}
+
+
+##################################################################################
 # PROVIDERS
 ##################################################################################
 
@@ -8,7 +36,31 @@ provider "aws" {
   region     = var.region
 }
 
+##################################################################################
+# DATA
+##################################################################################
 
+data "aws_availability_zones" "available" {}
+
+data "aws_ami" "aws-linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-hvm*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
 
 ##################################################################################
 # RESOURCES
